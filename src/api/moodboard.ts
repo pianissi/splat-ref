203d4addd4b99f0a81be0c7fc0459d15472c57be
pianissi <132866503+pianissi@users.noreply.github.com>
2025-03-26 02:21:33 +1100

    feat: add offline storage

--- src/api/moodboard.ts
+++ src/api/moodboard.ts
@@ -1,159 +1,89 @@
+import { openDB, deleteDB, wrap, unwrap, DBSchema, IDBPDatabase } from 'idb';
+
 export interface MoodboardMini {
   moodboardId: number,
   moodboardName: string,
   thumbnail?: string,
 }
 
-export interface Moodboard {
-  moodboardId: number,
+export interface MoodboardObject {
+  moodboardId?: number,
   moodboardName: string,
-  moodboardData: object,
+  moodboardData: string,
   ownerId?: number;
   thumbnail?: string,
 }
 
-let db : IDBDatabase | null;
+interface MoodboardDB extends DBSchema {
+  "moodboards": {
+    key: string;
+    value: MoodboardObject;
+  };
+}
 
-export function addMoodboard(moodboard: Moodboard) {
+let db : IDBPDatabase<MoodboardDB> | null;
+
+const DB_NAME = "moodboardStore"
+
+export async function addMoodboard(moodboard: MoodboardObject) {
   if (db === null) {
     return null;
   }
-  const transaction = db.transaction("moodboard");
-  const moodboards = transaction.objectStore("moodboards");
 
   const moodboardObject = {
-    moodboardId: -1,
     moodboardName: moodboard.moodboardName,
     moodboardData: moodboard.moodboardData,
     ownerId: -1,
     thumbnail: moodboard.thumbnail
   }
 
-  const request = moodboards.add(moodboardObject);
-
-  return new Promise(function(resolve, reject) {
-    request.onsuccess = function() {
-      resolve(request.result);
-    }
-    request.onerror = function(event) {
-      db = null;
-      reject();
-    };
-  });
+  return await db.add("moodboards", moodboardObject);
 }
 
-export function getMoodboard(moodboardId: number) {
+export async function getMoodboard(moodboardId: number) {
   if (db === null) {
     return null;
   }
-  const transaction = db.transaction("moodboard");
-  const moodboards = transaction.objectStore("moodboards");
-
-  const request = moodboards.get(moodboardId);
-
-  return new Promise(function(resolve, reject) {
-    request.onsuccess = function() {
-      resolve(request.result);
-    }
-    request.onerror = function(event) {
-      db = null;
-      reject();
-    };
-  });
+  return await db.get("moodboards", IDBKeyRange.only(moodboardId));
 }
 
-export function getAllMoodboards() {
+export async function getAllMoodboards() {
   if (db === null) {
     return null;
   }
-  const transaction = db.transaction("moodboard");
-  const moodboards = transaction.objectStore("moodboards");
-
-  const request = moodboards.getAll();
-
-  return new Promise(function(resolve, reject) {
-    request.onsuccess = function() {
-      resolve(request.result);
-    }
-    request.onerror = function(event) {
-      db = null;
-      reject();
-    };
-  });
+  return await db.getAll("moodboards");
 }
 
-export function updateMoodboard(moodboardId: string, moodboard: Moodboard) {
+export async function updateMoodboard(moodboard: MoodboardObject) {
   if (db === null) {
     return null;
   }
-  const transaction = db.transaction("moodboard");
-  const moodboards = transaction.objectStore("moodboards");
-
-  const moodboardObject = {
-    moodboardId: moodboardId,
+  const moodboardObject : MoodboardObject = {
+    moodboardId: moodboard.moodboardId,
     moodboardName: moodboard.moodboardName,
     moodboardData: moodboard.moodboardData,
     ownerId: -1,
     thumbnail: moodboard.thumbnail
-  }
+  };
 
-  const request = moodboards.put(moodboardObject);
-
-  return new Promise(function(resolve, reject) {
-    request.onsuccess = function() {
-      resolve(request.result);
-    }
-    request.onerror = function(event) {
-      db = null;
-      reject();
-    };
-  });
+  return await db.put("moodboards", moodboardObject);
 }
 
-export function deleteMoodboard(moodboardId: string) {
+export async function deleteMoodboard(moodboardId: IDBKeyRange) {
   if (db === null) {
     return null;
   }
-  const transaction = db.transaction("moodboard");
-  const moodboards = transaction.objectStore("moodboards");
-
-  const request = moodboards.delete(moodboardId);
-
-  return new Promise(function(resolve, reject) {
-    request.onsuccess = function() {
-      resolve(request.result);
-    }
-    request.onerror = function(event) {
-      db = null;
-      reject();
-    };
-  });
+  return await db.delete("moodboards", moodboardId);
 }
 
-export function clearDb() {
-  const deleteRequest = indexedDB.deleteDatabase("moodboardStore");
+export async function clearDb() {
+  await deleteDB(DB_NAME);
 }
 
 export async function initDb() {
-  const openRequest = indexedDB.open("moodboardStore", 1);
-  
-  openRequest.onupgradeneeded = function(event) {
-    db = openRequest.result;
-
-    switch(event.oldVersion) {
-      case 0:
-        db.createObjectStore("moodboards", {keyPath: "moodboardId", autoIncrement: true});
+  db = await openDB<MoodboardDB>(DB_NAME, 1, {
+    upgrade(db) {
+      db.createObjectStore("moodboards", {keyPath: "moodboardId", autoIncrement: true});
     }
-  };
-  
-  return new Promise(function(resolve, reject) {
-    openRequest.onsuccess = function() {
-      db = openRequest.result;
-      resolve(db);
-    }
-    openRequest.onerror = function(event) {
-      db = null;
-      reject();
-    };
   });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,78 @@
 'use client'
+import { addMoodboard, clearDb, getAllMoodboards, initDb, MoodboardObject, MoodboardMini } from "@/api/moodboard";
 import { RoundContainer } from "@/components/RoundContainer";
+import { deleteDB } from "idb";
 import Link from "next/link";
 import { Dialog } from "radix-ui";
-import { useState } from "react";
-import { FiPlus, FiX } from "react-icons/fi";
-
-const mockData = [{"moodboardId": 1}, {"moodboardId": 2}];
+import { FormEventHandler, ReactElement, SyntheticEvent, useEffect, useState } from "react";
+import { FiPlus, FiTrash, FiX } from "react-icons/fi";
 
 export default function Home() {
-  const [moodboardIds, setMoodboardIds] = useState<MoodboardMini[]>([{
-    moodboardId: 1,
-    moodboardName: "Moodboard 1"
-  }]);
+  const [moodboards, setMoodboards] = useState<MoodboardMini[]>([]);
 
-  const moodboards = [];
-  for (const moodboardId of moodboardIds) {
-    moodboards.push(<MoodboardLink key={moodboardId.moodboardId}/>);
+  const moodboardsData = [];
+  for (const moodboard of moodboards) {
+    moodboardsData.push(
+      <MoodboardLink key={moodboard.moodboardId} moodboardId={moodboard.moodboardId}>
+        <div>
+          {moodboard.moodboardName}
+        </div>
+      </MoodboardLink>
+    );
   }
 
-  const addMoodboard = () => {
-    setMoodboardIds(
-      [
-        ...moodboardIds,
-        {moodboardId: moodboardIds.slice(-1)[0].moodboardId + 1}
-      ]
-    )
+  useEffect(() => {
+    async function init() {
+      await initDb();
+
+      const moodboards = await getAllMoodboards();
+
+      console.log(moodboards);
+
+      if (moodboards === null)
+        return;
+      const moodboardsData : MoodboardMini[] = [];
+      for (const moodboard of moodboards) {
+        let id = 0;
+        if (!moodboard.moodboardId)
+          id = -1;
+        else 
+          id = moodboard.moodboardId;
+        const moodboardData = {
+          moodboardId: id,
+          moodboardName: moodboard.moodboardName,
+          thumbnail: moodboard.thumbnail,
+        }
+
+        moodboardsData.push(moodboardData);
+      }
+
+      setMoodboards(moodboardsData);
+    }
+
+    init();
+
+    console.log("init");
+  }, []);
+
+  const handleAddMoodboard = (event: SyntheticEvent<HTMLFormElement>) => {
+    
+    const target = event.target as typeof event.target & {
+      name: {value: string};
+    };
+    console.log(target);
+
+    const newMoodboard : MoodboardObject = {
+      moodboardId: -1,
+      moodboardName: target.name.value,
+      moodboardData: ""
+    }
+
+    console.log(newMoodboard);
+
+    addMoodboard(newMoodboard);
+    console.log("yes");
+    
   };
 
   return (
@@ -41,45 +89,87 @@ export default function Home() {
       </div>
       <div className="flex flex-col p-4 justify-between h-full">
         <div className="flex p-4">
-          {moodboards}
+          {moodboardsData}
         </div>
-        <div className="flex flex-row-reverse">
-          <MoodboardDialog onClick={addMoodboard}/>
+        <div className="flex flex-row-reverse items-baseline">
+          <MoodboardDialog handleSubmit={handleAddMoodboard}/>
+          <DeleteDialog handleSubmit={() => {
+            clearDb();
+          }}/>
         </div>
       </div>
     </div>
   );
 }
 
-function MoodboardLink() {
-  return <Link className="m-2 h-fit flex shrink-0 p-6 bg-white hover:bg-slate-200 rounded-xl shadow-lg justify-center " href="/moodboard/1">To moodboard</Link>;
+function MoodboardLink({children, moodboardId}: {children: ReactElement, moodboardId: number}) {
+  return (
+    <Link className="m-2 h-fit flex shrink-0 p-6 shadow-gray-300 bg-white rounded-xl shadow-lg justify-center transition hover:bg-slate-200 hover:shadow-xl hover:shadow-gray-400" href={`/moodboard/${moodboardId.toString()}`}>
+      {children}
+    </Link>
+  );
 }
 
-function MoodboardDialog({onClick}: Props) {
+function MoodboardDialog({handleSubmit}: Props) {
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
-        <button className="block bg-white justify-center align-middle m-4 rounded-full shadow-sm border border-gray-300 shadow-gray-400 transition hover:bg-slate-200 hover:shadow-md hover:shadow-gray-500" onClick={onClick}>
-          <FiPlus className="m-2" size="1.5em"></FiPlus>
+        <button className="block bg-white justify-center align-middle m-4 rounded-full shadow-sm border border-gray-300 shadow-gray-400 transition hover:bg-slate-200 hover:shadow-md hover:shadow-gray-500">
+          <FiPlus className="m-2" size="4em"></FiPlus>
         </button>
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-[#000000a9]"/>
         <Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-5 rounded-lg">
           <Dialog.Title className="text-xl font-bold">Add moodboard</Dialog.Title>
-          <fieldset className="py-2 flex">
-            <label htmlFor="name" className="pr-2 py-2">
-              Name
-            </label>
-            <input id="name" className="unset p-2 rounded-md flex outline-gray-400 outline-dashed outline-1 focus:outline focus:outline-2 focus:gray-600" defaultValue="New Moodboard"/>
-          </fieldset>
-          <div className="flex justify-end">
-             <Dialog.Close asChild>
-               <button className="block bg-white justify-center align-middle m-2 p-2 rounded-full shadow-sm border border-gray-300 shadow-gray-400 transition hover:bg-slate-200 hover:shadow-md hover:shadow-gray-500" onClick={onClick}>
-                 Add
-               </button>
-             </Dialog.Close>
-           </div>
+          <form method="post" onSubmit={handleSubmit}>
+            <fieldset className="py-2 flex">
+              <label htmlFor="name" className="pr-2 py-2">
+                Name
+              </label>
+              <input id="name" className="unset p-2 rounded-md flex outline-gray-400 outline-dashed outline-1 focus:outline focus:outline-2 focus:gray-600" defaultValue="New Moodboard"/>
+            </fieldset>
+            <div className="flex justify-end">
+              <button type="submit" className="block bg-white justify-center align-middle m-2 p-2 rounded-full shadow-sm border border-gray-300 shadow-gray-400 transition hover:bg-slate-200 hover:shadow-md hover:shadow-gray-500">
+                Add
+              </button>
+            </div>
+          </form>
+           <Dialog.Close asChild>
+             <button className="block absolute top-2 right-2">
+               <FiX className="m-2" size="1.2em"></FiX>
+             </button>
+           </Dialog.Close>
+         </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
+}
+
+function DeleteDialog({handleSubmit}: {handleSubmit?: () => void}) {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <button className="block bg-red-500 justify-center h-fit align-middle m-1 rounded-full shadow-sm border border-gray-300 shadow-gray-400 transition hover:bg-slate-200 hover:shadow-md hover:shadow-gray-500">
+          <FiTrash className="m-2" size="1.5em"></FiTrash>
+        </button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-[#000000a9]"/>
+        <Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-5 rounded-lg">
+          <Dialog.Title className="text-xl font-bold">Delete all moodboards!</Dialog.Title>
+          <Dialog.Description className="text-md">Please be very sure you want to delete all moodboards!</Dialog.Description>
+          <form method="post" onSubmit={handleSubmit}>
+            <div className="flex justify-end">
+                
+                <button type="submit" className="block bg-white justify-center align-middle m-2 p-2 rounded-full shadow-sm border border-gray-300 shadow-gray-400 transition hover:bg-slate-200 hover:shadow-md hover:shadow-gray-500">
+                  Cancel
+                </button>
+                <button type="submit" className="block bg-red-500 justify-center align-middle m-2 p-2 rounded-full shadow-sm border border-gray-300 shadow-gray-400 transition hover:bg-red-400 hover:shadow-md hover:shadow-gray-500">
+                  Delete!
+                </button>
+            </div>
+          </form>
            <Dialog.Close asChild>
              <button className="block absolute top-2 right-2">
                <FiX className="m-2" size="1.2em"></FiX>
@@ -92,5 +182,5 @@ function MoodboardDialog({onClick}: Props) {
 }
 
 interface Props {
-  onClick?: ()=>void,
+  handleSubmit?: FormEventHandler<HTMLFormElement>,
 }
