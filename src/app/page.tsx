e80a5bc9741e3938a221c62bf76c5120ad28b5b2
pianissi <132866503+pianissi@users.noreply.github.com>
2025-03-25 21:49:54 +1100

    feat: add modal dialog to create moodboards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 'use client'
 import { RoundContainer } from "@/components/RoundContainer";
 import Link from "next/link";
+import { Dialog } from "radix-ui";
 import { useState } from "react";
-import { FiPlus } from "react-icons/fi";
+import { FiPlus, FiX } from "react-icons/fi";
 
 const mockData = [{"moodboardId": 1}, {"moodboardId": 2}];
 
-interface MoodboardId {
-  moodboardId: number
-}
-
 export default function Home() {
-  const [moodboardIds, setMoodboardIds] = useState<MoodboardId[]>([{moodboardId: 1}, {moodboardId: 2}]);
+  const [moodboardIds, setMoodboardIds] = useState<MoodboardMini[]>([{
+    moodboardId: 1,
+    moodboardName: "Moodboard 1"
+  }]);
 
   const moodboards = [];
   for (const moodboardId of moodboardIds) {
@@ -28,16 +28,24 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col p-4 justify-between h-screen w-screen">
-      <div className="flex p-4">
-        {moodboards}
-      </div>
-      <div className="flex flex-row-reverse">
+    <div className="flex flex-col justify-start h-screen w-screen bg-gray-100">
+      <div className="flex px-2 justify-between items-center bg-gray-100 shadow-md border-b border-gray-300">
+        <div className="text-2xl m-4 font-bold h-fit w-auto">
+          Your Moodboards
+        </div>
         <RoundContainer hoverable={true}>
-          <button className="block" onClick={addMoodboard}>
-            <FiPlus className="m-2" size="1.5em"></FiPlus>
-          </button>
+          <div className="m-2">
+            Login
+          </div>
         </RoundContainer>
+      </div>
+      <div className="flex flex-col p-4 justify-between h-full">
+        <div className="flex p-4">
+          {moodboards}
+        </div>
+        <div className="flex flex-row-reverse">
+          <MoodboardDialog onClick={addMoodboard}/>
+        </div>
       </div>
     </div>
   );
@@ -45,4 +53,44 @@ export default function Home() {
 
 function MoodboardLink() {
   return <Link className="m-2 h-fit flex shrink-0 p-6 bg-white hover:bg-slate-200 rounded-xl shadow-lg justify-center " href="/moodboard/1">To moodboard</Link>;
+}
+
+function MoodboardDialog({onClick}: Props) {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <button className="block bg-white justify-center align-middle m-4 rounded-full shadow-sm border border-gray-300 shadow-gray-400 transition hover:bg-slate-200 hover:shadow-md hover:shadow-gray-500" onClick={onClick}>
+          <FiPlus className="m-2" size="1.5em"></FiPlus>
+        </button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-[#000000a9]"/>
+        <Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-5 rounded-lg">
+          <Dialog.Title className="text-xl font-bold">Add moodboard</Dialog.Title>
+          <fieldset className="py-2 flex">
+            <label htmlFor="name" className="pr-2 py-2">
+              Name
+            </label>
+            <input id="name" className="unset p-2 rounded-md flex outline-gray-400 outline-dashed outline-1 focus:outline focus:outline-2 focus:gray-600" defaultValue="New Moodboard"/>
+          </fieldset>
+          <div className="flex justify-end">
+             <Dialog.Close asChild>
+               <button className="block bg-white justify-center align-middle m-2 p-2 rounded-full shadow-sm border border-gray-300 shadow-gray-400 transition hover:bg-slate-200 hover:shadow-md hover:shadow-gray-500" onClick={onClick}>
+                 Add
+               </button>
+             </Dialog.Close>
+           </div>
+           <Dialog.Close asChild>
+             <button className="block absolute top-2 right-2">
+               <FiX className="m-2" size="1.2em"></FiX>
+             </button>
+           </Dialog.Close>
+         </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
+}
+
+interface Props {
+  onClick?: ()=>void,
 }
--- src/components/RoundContainer.tsx
+++ src/components/RoundContainer.tsx
@@ -1,9 +1,8 @@
 'use client';
 import { ReactNode } from "react";
-import { Props } from "../app/moodboard/[moodboardId]/page";
 
 export function RoundContainer({ children, hoverable = false }: Props) {
-  return <div className={`bg-white justify-center align-middle m-4 rounded-full shadow-md border border-gray-500 ${hoverable && 'hover:bg-slate-200'}`}>{children}</div>;
+  return <div className={`bg-white justify-center align-middle m-4 rounded-full shadow-sm border border-gray-300 shadow-gray-400 transition ${hoverable && 'hover:bg-slate-200 hover:shadow-md hover:shadow-gray-500'}`}>{children}</div>;
 }
 interface Props {
   children?: ReactNode,
